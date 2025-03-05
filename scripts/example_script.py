62a366aedb45a2ce89004c252ca4b3c852dd02d8
Surabhi Jain <surabhijain3397@gmail.com>
2025-03-05 12:56:35 +0530

    Added a placeholder bioinformatics script

---
+++ scripts/example_script.py
@@ -0,0 +1,4 @@
+# Example Bioinformatics Script
+# This script will later contain functions for sequence analysis, molecular docking, or other bioinformatics tasks.
+
+print("Hello, Bioinformatics!")
